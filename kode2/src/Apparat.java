1da84b60bd4998c9ed0ac08424efc57b93ae84c0
olejorgh <olejorgh@stud.ntnu.no>
2019-03-16 10:40:43 +0100

    Created Apparat class

---
+++ kode2/src/Apparat.java
@@ -0,0 +1,32 @@
+public class Apparat {
+
+    static private int apparatID = 1;
+    private String navn = null;
+    private String beskrivelse = null;
+
+
+    public Apparat(String navn, String beskrivelse) {
+        this.navn = navn;
+        this.beskrivelse = beskrivelse;
+        this.personID = apparatID;
+        apparatID += 1;
+    }
+
+    public Apparat(String navn) {
+        this.navn = navn;
+        this.personID = apparatID;
+        apparatID += 1;
+    }
+
+    public String getNavn(){
+        return this.navn;
+    }
+
+    public int getBeskrivelse()){
+        return this.beskrivelse
+    }
+
+    public void setBeskrivelse(Strimg beskrivelse){
+        this.beskrivelse = beskrivelse;
+    }
+}
